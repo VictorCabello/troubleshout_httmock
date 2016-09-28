14bafbbd41da6ae20b265e4a8d4fe6771fd66513
Victor Cabello <vmeca87@gmail.com>
2016-09-27 22:33:11 -0500

    add request to api server

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ This is only a test of how to use
 requests module and multiprocessing
 """
 import os, traceback
+import requests
 
 def main():
-    print 'hello'
+    r = requests.get('https://api.github.com/orgs/octokit/repos')
+    list = r.json()
+    for item in list:
+        owner = item['owner']['login']
+        next_r = requests.get("https://api.github.com/users/{0}".format(owner))
+        print "{name} ({owner})".\
+            format(name=item['name'], owner=next_r.json()['login'])
 
 if __name__ == '__main__':
     try:
