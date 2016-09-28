eaba7b25028b0117897de8114f991f26a3d012b0
Victor Cabello <vmeca87@gmail.com>
2016-09-27 23:19:26 -0500

    Add multiprocessing

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ requests module and multiprocessing
 """
 import os, traceback
 import requests
+from multiprocessing import Pool
+
+def getowner(repo):
+    owner = repo['owner']['login']
+    r = requests.get("https://api.github.com/users/{0}".format(owner))
+    return "{repo} ({owner})".format(repo=repo['name'], owner=r.json()['login'])
 
 def main():
     r = requests.get('https://api.github.com/orgs/octokit/repos')
-    list = r.json()
-    for item in list:
-        owner = item['owner']['login']
-        next_r = requests.get("https://api.github.com/users/{0}".format(owner))
-        print "{name} ({owner})".\
-            format(name=item['name'], owner=next_r.json()['login'])
+    repos = [repo for repo in r.json()]
+    p = Pool(4)
+    print p.map(getowner, repos)
 
 if __name__ == '__main__':
     try:
