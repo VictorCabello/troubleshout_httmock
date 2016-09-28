21ca88f505bec06164d75c5daf3d725fad248e2a
Victor Cabello <vmeca87@gmail.com>
2016-09-27 22:12:13 -0500

    add structure to the main module

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-#! /bin/python
-print 'hi'
+#!/usr/bin/env python
+"""
+This is only a test of how to use
+requests module and multiprocessing
+"""
+import os
+
+def main():
+    print 'hello'
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception, e:
+        print str(e)
+        traceback.print_exc()
+        os.__exit(1)
